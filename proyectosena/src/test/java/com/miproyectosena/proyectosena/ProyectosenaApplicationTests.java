9e8995bb51d7de0957cd71b085994530399804e9
jjime0216 <jeuli_98@hotmail.com>
2025-04-15 23:40:55 -0500

    Rama para probar el backend del proyecto

---
+++ proyectosena/src/test/java/com/miproyectosena/proyectosena/ProyectosenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miproyectosena.proyectosena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
